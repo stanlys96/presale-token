3262cac97b27ed26d567799400c3f477dc1942cb
stanlys96 <stanlyskwok@gmail.com>
2023-07-12 10:59:52 +0700

    walao

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,28 @@
 import "../app/globals.css";
 import { MoralisProvider } from "react-moralis";
 import { AppProps } from "next/app";
+import { Mainnet, DAppProvider, Goerli } from "@usedapp/core";
+import { getDefaultProvider } from "ethers";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <MoralisProvider
-        // appId={APP_ID}
-        // serverUrl={SERVER_URL}
-        initializeOnMount={false}
+      <DAppProvider
+        config={{
+          readOnlyChainId: Mainnet.chainId,
+          readOnlyUrls: {
+            // [Mainnet.chainId]: getDefaultProvider("mainnet"),
+            [Goerli.chainId]:
+              "https://goerli.infura.io/v3/11d0a5344888470393248b1c55105a8c",
+          },
+          notifications: {
+            expirationPeriod: 1000,
+            checkInterval: 1000,
+          },
+        }}
       >
         <Component {...pageProps} />
-      </MoralisProvider>
+      </DAppProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
-import { useEffect } from "react";
-import { useMoralis } from "react-moralis";
+import { useEffect, useState } from "react";
+import { useMoralis, useWeb3Contract } from "react-moralis";
+import { useEthers, useNotifications } from "@usedapp/core";
 import Swal from "sweetalert2";
+import ProgressBar from "@ramonak/react-progress-bar";
+import supermanPresale from "../constants/SupermanPresale.json";
+import supermanCoin from "../constants/SupermanCoin.json";
+import { ethers, utils } from "ethers";
+import { Icon } from "@iconify/react";
+import { useGetProgress, useBuyTokenEth } from "@/hooks";
 
 const truncateStr = (fullStr: any, strLen: any) => {
   if (fullStr.length <= strLen) return fullStr;
@@ -18,73 +25,86 @@ const truncateStr = (fullStr: any, strLen: any) => {
 };
 
 export default function HomePage() {
-  const {
-    enableWeb3,
-    isWeb3Enabled,
-    isWeb3EnableLoading,
-    account,
-    Moralis,
-    deactivateWeb3,
-  } = useMoralis();
+  const tokenProgress = useGetProgress();
+  const { send: buyTokenEth, state: buyTokenEthState } = useBuyTokenEth();
+  const decimals = 18;
+  const [progress, setProgress] = useState(0);
+  const [price, setPrice] = useState(0.1);
+  const { account, activateBrowserWallet, deactivate, chainId } = useEthers();
+
+  useEffect(() => {
+    console.log(tokenProgress?.value.toString(), "<<<< WALAO");
+  }, [account, tokenProgress]);
   return (
     <div className="h-[100vh] bg-white the-container text-white">
       <nav className="max-w-[1200px] w-full mx-auto h-[15vh]">
         <ul className="flex justify-between py-4">
           <li>Walao</li>
-          <li>
-            <button
-              className="p-2 bg-blue text-white rounded-[15px]"
-              onClick={async () => {
-                if (!isWeb3Enabled) {
-                  const res = await enableWeb3();
-                  console.log(res, "<<<");
-                  if (!res) {
-                    Swal.fire({
-                      title: "You don't have Metamask downloaded!",
-                      html: "Please download Metamask at <a href='https://metamask.io/download/' target='_blank'>https://metamask.io/download/</a>",
-                    });
-                  }
-                  if (typeof window !== "undefined" && res) {
+          <li className="flex items-stretch">
+            {!account && (
+              <button
+                onClick={async () => {
+                  activateBrowserWallet();
+                  if (typeof window !== "undefined") {
                     window.localStorage.setItem("connected", "injected");
                   }
-                } else {
-                  await deactivateWeb3();
-                }
-              }}
-            >
-              {account
-                ? `Connected to ${account.slice(0, 6)}... ${account.slice(
-                    account.length - 4
-                  )}`
-                : "Metamask Login"}
-            </button>
+                }}
+                className="selectWalletBtn"
+              >
+                Connect Metamask
+              </button>
+            )}
+            {account && (
+              <div
+                className="metamaskIconContainer"
+                onClick={async () => {
+                  deactivate();
+                  if (typeof window !== "undefined") {
+                    window.localStorage.removeItem("connected");
+                  }
+                }}
+              >
+                <Icon icon="logos:metamask-icon" />
+              </div>
+            )}
+            {account && (
+              <div className="accountContainer">
+                {account.slice(0, 6)}...
+                {account.slice(account.length - 4)}
+              </div>
+            )}
           </li>
         </ul>
       </nav>
       <div className="max-w-[1000px] w-full mx-auto h-[85vh]">
         <div className="grid grid-cols-2 gap-x-8 h-full">
           <div className="flex flex-col gap-6 justify-center items-center h-full">
-            <p className="text-left">
-              Welcome to the Presale of Boston Dynamics Inu
-            </p>
+            <p className="text-left">Welcome to the Presale of Superman Coin</p>
             <p>
-              Buy $BDINU tokens at a very discounted price in the Presale. Swap
-              ETH for $BDINU without any fees at the lowest price. During the
-              Presale $BDINU is available for only $0.21 compared to the public
+              Buy $SPC tokens at a very discounted price in the Presale. Swap
+              ETH for $SPC without any fees at the lowest price. During the
+              Presale $SPC is available for only $0.21 compared to the public
               sale for $0.53
             </p>
             <img className="w-[240px] h-[240px]" src="bdinu.png" />
           </div>
           <div className="flex flex-col mt-[65px] items-center gap-3 bg-black h-fit p-4 rounded-[15px]">
             <p>PRESALE ENDING SOON</p>
-            <p>1 BDINU = $0.21 USDT</p>
-            <p>69.06% SOLD</p>
-            <p>WALAO</p>
+            <p>1 SPC = $0.21 USDT</p>
+            <p>0% SOLD</p>
+            <ProgressBar
+              className="w-full"
+              completed={parseInt(tokenProgress?.toString() ?? "0")}
+            />
             <p>ETH RAISED - 107.564 ETH</p>
-            <button className="bg-blue w-full p-3 rounded-[10px] text-white">
-              Buy With USDT
-            </button>
-            <button className="bg-blue w-full p-3 rounded-[10px] text-white">
+            <button
+              onClick={async () => {
+                const amountAsWei = utils.parseEther("0.1");
+                console.log(amountAsWei.toString(), "<<<");
+                buyTokenEth({ value: amountAsWei.toString() });
+              }}
+              className="bg-blue w-full p-3 rounded-[10px] text-white"
+            >
               Buy With ETH
             </button>
           </div>
